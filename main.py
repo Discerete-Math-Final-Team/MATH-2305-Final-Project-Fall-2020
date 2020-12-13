8185ed9d9018955e69de08c957c9a51979a84286
Saucemannn <75696257+Saucemannn@users.noreply.github.com>
2020-12-13 12:08:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import networkx as nx
 from functions import * 
 from drawing import *
 from algorithms import *
-
+'''
+will open and return a file object in this case a graph to be read,
+if file is not found will raise an error
+NetworkX will then generate a plot for the chosen file
+It will then ask for an input for a starting vertex and will
+ensure returning a hashable type
+'''
 graph_data = open('test-graphs/G4.txt','r') #read in the graph you want to test G1-G4
 
 G = nx.read_weighted_edgelist(graph_data, nodetype = int) #create initial graph 
